ab64af72c4bc5720110b9632cb6d24c05694fc41
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-07 21:21:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if 60 < speed || speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	60 < speed || speed < 40 ? true : false
 end
 	
 
